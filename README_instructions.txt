db5ceb10754723f15b93b7e76eef7527ca12ceb9
Akshay Karve <akshay.karve@stonybrook.edu>
2021-02-16 04:27:15 -0500

    Initial work on scripts. Google api translates 2/41 2 minute files well 7 poorly and stops at file 11. Check file 11 if the professor stopped talking for 2 minutes.

---
+++ README_instructions.txt
@@ -0,0 +1,2 @@
+use ffmpeg to convert .mp4 to .wav
+use audioTranscriptionFromFile.py to convert from .wav to 180s chuncks of text
---
+++ audioTranscriptionFromFile.py
@@ -0,0 +1,35 @@
+import os
+from os import walk
+
+dirname = os.path.dirname(__file__)+"\\AudioFiles\\Test1_TwoMinuteSegments\\"
+# filename = os.path.join(dirname,'./AudioFiles/Test1_TwoMinuteSegments/Test1000.wav')
+
+_, _, filenames = next(walk('./AudioFiles/Test1_TwoMinuteSegments/'))
+
+import wave
+
+import speech_recognition as sr
+
+for fn in filenames:
+    n = os.path.join(dirname,fn)
+    # print(fn)
+    # print(n)
+    r = sr.Recognizer()
+    with sr.AudioFile(n) as source:
+        audio = r.record(source)
+        print("\n"+fn)
+        print(r.recognize_google(audio, language="en-US"))
+
+# print(filename)
+# d = 120
+# o = 316
+
+# with sr.AudioFile(filename) as source:
+    # audio = r.record(source, duration=d, offset=o)
+    # print(r.recognize_google(audio))
+
+    # for i in range(3):
+    #     print("the offset is "+str(o)+" the duration is "+str(d)+".\n")
+    #     audio = r.record(source, duration=d, offset=o)
+    #     print(r.recognize_google(audio))
+    #     o+=d
