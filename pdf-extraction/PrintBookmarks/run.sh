7d42ac69890d4029a52897ff71dcd428c229fe7e
Giorgian Borca-Tasciuc <giorgian.borca-tasciuc-accounts@protonmail.com>
2021-03-14 10:16:26 -0400

    Created Maven PDF Extraction Project

--- pdf-extraction/PrintBookmarks/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-CLASSPATH=pdfbox2.jar:./:/usr/share/java/commons-logging.jar:fontbox2.jar
-java -cp $CLASSPATH GetBookmarks "$1"
--- pdf-extraction/pdfextraction/src/main/java/com/greekengineering/lecturetoc/App.java
+++ pdf-extraction/pdfextraction/src/main/java/com/greekengineering/lecturetoc/App.java
@@ -1,10 +1,12 @@
+package com.greekengineering.lecturetoc;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDDocumentOutline;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineItem;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineNode;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
@@ -13,58 +15,53 @@ import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.text.PDFTextStripper;
 
-public class GetBookmarks
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+public class App
 {
-    /**
-     * This will print the documents data.
-     *
-     * @param args The command line arguments.
-     *
-     * @throws Exception If there is an error parsing the document.
-     */
+    public static boolean SORT_BY_BEADS = false;
+    public static boolean SORT_BY_POSITION = true;
     public static void main(String[] args) throws Exception
     {
-        if (args.length != 1)
+        if (args.length != 2)
         {
+            System.out.println("Need two files.");
             System.exit(1);
         }
+        final String pdfPath = args[0];
+        final String csvPath = args[1];
 
-        PDDocument doc = PDDocument.load(new File(args[0]));
+        PDDocument doc = PDDocument.load(new File(pdfPath));
         PDDocumentOutline root = doc.getDocumentCatalog().getDocumentOutline();
         ArrayList<PDOutlineItem> bookmarks = new ArrayList<>();
         getBookmarks(root, bookmarks);
         PDFTextStripper strip = new PDFTextStripper();
         ArrayList<String> texts = new ArrayList<>();
-//        strip.setShouldSeparateByBeads(true);
-        strip.setSortByPosition(true);
-//        PDOutlineItem testBookmark = bookmarks.get(10);
-//        COSDictionary dict = testBookmark.getCOSObject();
-//        for (Map.Entry<COSName, COSBase> cur: dict.entrySet())
-//            System.out.println(cur.getKey().getName());
-//        for (Map.Entry<COSName, COSBase> cur: dict.entrySet())
-//            System.out.println(cur.getKey().getName());
-//
-//        System.exit(0);
+        strip.setShouldSeparateByBeads(SORT_BY_BEADS);
+        strip.setSortByPosition(SORT_BY_POSITION);
         for (int i = 0; i < bookmarks.size() - 1; ++i) {
             strip.setStartBookmark(bookmarks.get(i));
             strip.setEndBookmark(bookmarks.get(i + 1));
             try {
-                texts.add(strip.getText(doc).trim().replaceAll("\\s\\s*", " ").replaceAll(",", "_GREEK_ENGINEERING_COMMA_INTERNAL_USAGE_"));
-            } catch (IOException e) {
-                texts.add("");
-                System.out.println("Skipping " + bookmarks.get(i).getTitle());
-            }
+                texts.add(strip.getText(doc).trim().replaceAll("\\s\\s*", " "));
+                        } catch (IOException e) {
+                            texts.add("");
+                            System.out.println("Skipping " + bookmarks.get(i).getTitle());
+                        }
         }
         strip.setStartBookmark(bookmarks.get(bookmarks.size() - 1));
         strip.setEndBookmark(null);
         texts.add(strip.getText(doc).trim().replaceAll("\\s\\s*", " "));
 
-        try (PrintWriter writer = new PrintWriter(new FileWriter("chapters.csv"))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvPath))) {
+            CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT);
             for (int i = 0; i < bookmarks.size() && i < texts.size(); ++i) {
-                final String title = bookmarks.get(i).getTitle().replace("\"", "\\\"");
-                final String text = texts.get(i).replace("\"", "\\\"");
-                writer.printf("\"%s\",\"%s\"\n", title, text);
+                final String title = bookmarks.get(i).getTitle();
+                final String text = texts.get(i);
+                printer.printRecord(title, text);
             }
+            printer.flush();
         }
     }
 
