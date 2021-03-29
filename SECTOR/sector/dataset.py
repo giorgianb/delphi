bd06bf18a50191e8168a6921fa0013aa14c1b99f
Giorgian Borca-Tasciuc <giorgian.borca-tasciuc-accounts@protonmail.com>
2021-03-29 16:54:48 -0400

    Added dataset. Now, sentences from within a topic are kept grouped together. The order of the topics during each epochs changes, along with the order of the sentences within each epoch

--- SECTOR/sector/dataset.py
+++ SECTOR/sector/dataset.py
@@ -9,48 +9,7 @@ import random
 
 csv.field_size_limit(sys.maxsize)
 
-DATA_FILE = "data.csv"
-
-# shuffle the order of the sentences within each topic group
-# also shuffle the order of the topics
-@tf.function
-def within_topic_shuffle(batch):
-    print(type(batch))
-    print(batch.shape)
-    sentences = tf.unstack(sentences)
-    topics = tf.unstack(topics)
-    s_groups = []
-    t_groups = []
-    prev_topic = topics[0]
-    current_s_group = [sentences[0]]
-    current_t_group = [topics[0]]
-    for s, t in zip(sentences[1:], topics[1:]):
-        if t.numpy() == prev_topic.numpy():
-            current_s_group.append(s)
-            current_t_group.append(t)
-        else:
-            s_groups.append(current_s_group)
-            t_groups.append(current_t_group)
-            prev_topic = t
-            current_s_group = [s]
-            current_t_group = [t]
-    s_groups.append(current_s_group)
-    t_groups.append(current_t_group)
-
-    for s_group in s_groups:
-        random.shuffle(s_group)
-
-    s = random.getstate()
-    random.setstate(s)
-    random.shuffle(s_groups)
-    random.setstate(s)
-    random.shuffle(t_groups)
-
-    new_sentences = [s for group in s_groups for s in group]
-    new_topics = [t for group in t_groups for t in group]
-
-    return tf.stack(new_sentences), tf.stack(new_topics)
-
+DATA_FILE = "micro-data.csv"
 def tokenize_and_lemmatize_data(data):
     nlp = stanza.Pipeline('en', processors='tokenize,lemma')
     rows = []
@@ -68,11 +27,28 @@ def chunks(lst, n):
 def flatten(lst):
     return (item for sublist in lst for item in sublist)
 
+def group_by_topic(lst, topics):
+    prev_topic = topics[0]
+    cur_group = [lst[0]]
+    groups = []
+
+    for item, topic in zip(lst[1:], topics[1:]):
+        if topic == prev_topic:
+            cur_group.append(item)
+        else:
+            groups.append(cur_group)
+            cur_group = [item]
+            prev_topic = topic
+
+    groups.append(cur_group)
+    return groups
+
+
 # assumes data is tokenized and lemmatized
 def make_dataset(data, processor, test_size=0.2, batch_sample_size=100, random_state=42):
     sentence_sequence = []
-    topic_sequence = []
     sentence_texts = []
+    topic_sequence = []
 
     topic_counter = 0
     topics = {}
@@ -83,58 +59,78 @@ def make_dataset(data, processor, test_size=0.2, batch_sample_size=100, random_s
 
         for sentence, text in zip(parsed, sentences):
             sentence_sequence.append(processor(sentence))
-            sentence_texts.append(text)
+            sentence_texts.append(' '.join(text))
             topic_sequence.append(topics[topic])
 
-    train_sentences = []
-    test_sentences = []
-    train_topics = []
-    test_topics = []
-    train_text = []
-    test_text = []
+    sentence_groups = group_by_topic(sentence_sequence, topic_sequence)
+    sentence_text_groups = group_by_topic(sentence_texts, topic_sequence)
+    topic_groups = group_by_topic(topic_sequence, topic_sequence)
 
-    sentence_sequence = list(chunks(sentence_sequence, batch_sample_size))
-    topic_sequence = list(chunks(topic_sequence, batch_sample_size))
-    sentence_texts = list(chunks(sentence_texts, batch_sample_size))
-    for ss, ts, st in zip(sentence_sequence, topic_sequence, sentence_texts):
-        n = len(ss)
+    train_sentence_groups = []
+    train_sentence_text_groups = []
+    train_topic_groups = []
+    test_sentences = []
+    test_sentence_texts = []
+    test_topics = []
+    random.seed(random_state)
+    for s_group, st_group, t_group in zip(sentence_groups, sentence_text_groups, topic_groups):
+        n = len(s_group)
+        assert len(s_group) == len(st_group)
+        assert len(st_group) == len(t_group)
         tr_indices, te_indices = train_test_split(
                 tuple(range(n)), 
                 test_size=test_size, 
-                random_state=random_state
-                )
-        tr_indices = sorted(tr_indices)
-        te_indices = sorted(te_indices)
+                random_state=random.randint(0, int(2**32))
+        )
 
-        train_sentences.extend(ss[i] for i in tr_indices)
-        test_sentences.extend(ss[i] for i in te_indices)
-        train_topics.extend(ts[i] for i in tr_indices)
-        test_topics.extend(ts[i] for i in te_indices)
-        train_text.extend(st[i] for i in tr_indices)
-        test_text.extend(st[i] for i in te_indices)
-
-    sentence_sequence = list(flatten(sentence_sequence))
-    topic_sequence = list(flatten(topic_sequence))
-    sentence_texts = list(flatten(sentence_texts))
-
+        train_sentences = [s_group[i] for i in tr_indices]
+        train_sentence_texts = [st_group[i] for i in tr_indices]
+        train_topics = [t_group[i] for i in tr_indices]
+        train_sentence_groups.append(train_sentences)
+        train_sentence_text_groups.append(train_sentence_texts)
+        train_topic_groups.append(train_topics)
+        test_sentences.extend(s_group[i] for i in te_indices)
+        test_topics.extend(t_group[i] for i in te_indices)
+        test_sentence_texts.extend(st_group[i] for i in te_indices)
 
     # Generate the dataset
     def generate_dataset():
+        new_topic_order = list(range(len(train_sentence_groups)))
+        random.shuffle(new_topic_order)
+        tsg = train_sentence_groups
+        tstg = train_sentence_text_groups
+        ttg = train_topic_groups
+        for i in new_topic_order:
+            new_sentence_order = list(range(len(tsg[i])))
+            random.shuffle(new_sentence_order)
+            for j in new_sentence_order:
+                yield tsg[i][j], tstg[i][j], ttg[i][j]
 
-    print("TOTAL SENTENCES: {}".format(len(sentence_sequence)))
+    EMBEDDING_DIM = len(sentence_sequence[0])
+    train_dataset = tf.data.Dataset.from_generator(
+            generate_dataset,
+            output_signature=(
+                tf.TensorSpec(shape=(EMBEDDING_DIM,), dtype=tf.float32),
+                tf.TensorSpec(shape=(), dtype=tf.string),
+                tf.TensorSpec(shape=(), dtype=tf.int32)
+            )
+    )
+
     sentence_sequence = tf.data.Dataset.from_tensor_slices(sentence_sequence)
     topic_sequence = tf.data.Dataset.from_tensor_slices(topic_sequence)
-    train_sentences = tf.data.Dataset.from_tensor_slices(train_sentences)
-    train_topics = tf.data.Dataset.from_tensor_slices(train_topics)
+    sentence_texts = tf.data.Dataset.from_tensor_slices(sentence_texts)
+    dataset = tf.data.Dataset.zip((sentence_sequence, sentence_texts, topic_sequence))
+
     test_sentences = tf.data.Dataset.from_tensor_slices(test_sentences)
     test_topics = tf.data.Dataset.from_tensor_slices(test_topics)
+    test_sentence_texts = tf.data.Dataset.from_tensor_slices(test_sentence_texts)
+    test_dataset = tf.data.Dataset.zip((
+        test_sentences, 
+        test_sentence_texts, 
+        test_topics
+        ))
 
-    dataset = tf.data.Dataset.zip((sentence_sequence, topic_sequence))
-    train_dataset = tf.data.Dataset.zip((train_sentences, train_topics))
-    test_dataset = tf.data.Dataset.zip((test_sentences, test_topics))
-
-
-    return topics, dataset, train_dataset, test_dataset, train_text, test_text
+    return topics, dataset, train_dataset, test_dataset
 
 with open(DATA_FILE) as f:
     data = list(csv.reader(f))
@@ -146,11 +142,9 @@ with open(DATA_FILE) as f:
     dataset = None 
     train_dataset = None
     test_dataset = None
-    train_text = None
-    test_text = None
 
 def initialize_dataset_bloom(n_hash_functions, sentence_embedding_size, random_state=42):
-    global topics, dataset, train_dataset, test_dataset, train_text, test_text
+    global topics, dataset, train_dataset, test_dataset
     processor = embeddings.BloomFilter(n_hash_functions, sentence_embedding_size)
     datasets = make_dataset(data, processor, random_state=random_state)
-    topics, dataset, train_dataset, test_dataset, train_text, test_text = datasets
+    topics, dataset, train_dataset, test_dataset = datasets
